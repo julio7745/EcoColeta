8f5b17856733bd3af0c6bc4b940aec8be7671839
Júlio César Queiroz Carvalho <juliocarvalho5998@gmail.com>
2023-10-02 18:32:51 -0300

    add icone  + melhoria do css

--- ecocoleta-frontend/src/components/header/header.tsx
+++ ecocoleta-frontend/src/components/header/header.tsx
@@ -4,6 +4,8 @@ import '../../styles/header/header.css';
 
 import Menu from '../menu/menu'
 
+import logo from '../../assets/imgs/icos/plantinha.png';
+
 interface HeaderProps {
   currentPage: string,
   setCurrentPage: (page: string) => void,
@@ -13,7 +15,10 @@ function Header(props: HeaderProps) {
 
   return(
     <div className='header'>
-      <div className="title">Eco Coleta</div>
+      <div className='container'>
+        <div className="title">Eco Coleta</div>
+        <img src={logo} alt="LOGO"/>
+      </div>
       <Menu {...{
         currentPage: props.currentPage,
         setCurrentPage: props.setCurrentPage, 
